1f8ab9535656ac0ad9080293578f06caa20a2fdd
chima1968 <chimarokeumunna98@gmail.com>
2025-03-18 14:36:03 +0100

    updates

--- src/components/sign_in.jsx
+++ src/components/sign_in.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "./userContext";
-import { Dashboard } from "./Dashboard";
+import { Dashboard } from "./dashboard";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
